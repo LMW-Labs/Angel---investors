23cf8e527b6d2fbaf09731c23858faa0e9c60711
Austin <199020683+LMW-Labs@users.noreply.github.com>
2026-04-13 00:04:11 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import InvestorDashboard from './InvestorDashboard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <InvestorDashboard />
+  </React.StrictMode>,
+)
